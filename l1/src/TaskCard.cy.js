d2d277d18e6e1f2b15754dff80576a2114136fda
Hari Gopal <mail@harigopal.in>
2023-04-03 15:51:09 +0530

    Improve test descriptions

--- l1/src/TaskCard.cy.js
+++ l1/src/TaskCard.cy.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import TaskCard from './TaskCard'
+import React from "react";
+import TaskCard from "./TaskCard";
 
-describe('<TaskCard />', () => {
-  it('renders name', () => {
-    cy.mount(<TaskCard assigneeName="Avishek"/>)
-    cy.contains("Avishek")
-  })
-  it('renders due date and does not renders completed on date', () => {
-    cy.mount(<TaskCard dueDate="22nd March" />)
-    cy.contains("Due on: 22nd March", { matchCase: false })
-    cy.contains("completed on: 22nd March", { matchCase: false }).should('not.exist')
-  })
-  it('renders completed on date and does not renders due date', () => {
-    cy.mount(<TaskCard completedAtDate="22nd March" />)
-    cy.contains("Completed on: 22nd March", { matchCase: false })
-    cy.contains("Due on: 22nd March", { matchCase: false }).should('not.exist')
-  })
-})
+describe("The <TaskCard> component", () => {
+  it("shows the name passed with `assigneeName` prop", () => {
+    cy.mount(<TaskCard assigneeName="Avishek" />);
+    cy.contains("Avishek");
+  });
+
+  it('shows only "Due on:" date when given the `dueDate` prop', () => {
+    cy.mount(<TaskCard dueDate="22nd March" />);
+    cy.contains("Due on: 22nd March", { matchCase: false });
+    cy.contains("completed on: 22nd March", { matchCase: false }).should(
+      "not.exist"
+    );
+  });
+
+  it('shows only "Completed on:" date when given the `completedAtDate` prop', () => {
+    cy.mount(<TaskCard completedAtDate="22nd March" />);
+    cy.contains("Completed on: 22nd March", { matchCase: false });
+    cy.contains("Due on: 22nd March", { matchCase: false }).should("not.exist");
+  });
+});
