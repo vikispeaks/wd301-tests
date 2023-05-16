e793c6cd20a73285ab37e0ae1d8e83969976769d
Vignesh Rajendran <vignesh.r.rajendran@oracle.com>
2023-05-17 00:31:54 +0530

    Update meaningful comments for tests

--- l5/cypress/e2e/todo.cy.ts
+++ l5/cypress/e2e/todo.cy.ts
@@ -5,17 +5,7 @@ const todoItems = [
     title: "Sample item 1",
     description: "item 1 description",
     dueDate: "2023-01-09",
-  },
-  {
-    title: "Sample item 2",
-    description: "item 2 description",
-    dueDate: "2023-01-09",
-  },
-  {
-    title: "Sample item 3",
-    description: "item 3 description",
-    dueDate: "2023-01-09",
-  },
+  }
 ];
 
 const addEntries = () => {
@@ -32,7 +22,7 @@ if (studentSubmissionUrl.endsWith("/")) {
   studentSubmissionUrl = studentSubmissionUrl.slice(0, -1);
 }
 
-describe("Level 5 milestone homepage", () => {
+describe("After signing in,", () => {
   beforeEach(() => {
     cy.visit(studentSubmissionUrl);
     cy.get("#username").clear();
@@ -42,16 +32,16 @@ describe("Level 5 milestone homepage", () => {
     cy.get("button[type='submit']").click();
   });
 
-  it("should have a <nav> element", () => {
+  it("the page should have a `<nav>` element", () => {
     cy.get("nav").should("be.visible");
   });
 
-  it("should have one (and only one) level one heading element", () => {
+  it("the page should have one (and only one) level one heading element", () => {
     cy.get("h1").should("have.length", 1);
   });
 });
 
-describe("Level 5 milestone tasks page", () => {
+describe("After signing in and visiting the `/tasks` page,", () => {
   beforeEach(() => {
     cy.visit(studentSubmissionUrl);
     cy.get("#username").clear();
@@ -62,12 +52,12 @@ describe("Level 5 milestone tasks page", () => {
     cy.visit(studentSubmissionUrl + "/tasks");
   });
 
-  it("can add new todo items", () => {
+  it("new `.TaskItem` elements should appear when adding items by filling in `#todoDueDate`, `#todoTitle`, `#todoDescription`,  and then clicking the `#addTaskButton`", () => {
     addEntries();
     cy.get(".TaskItem").should("have.length", todoItems.length);
   });
 
-  it("should not add empty item", () => {
+  it("the user should not be able to add items with necessary fields missing", () => {
     const todoItems = [
       {
         title: "Sample item 1",
@@ -92,21 +82,14 @@ describe("Level 5 milestone tasks page", () => {
     cy.get(".TaskItem").should("have.length", 0);
   });
 
-  it("should persist task items", () => {
+  it("reloading the page after adding some items should still show the items on the page", () => {
     addEntries();
     cy.reload();
     cy.get(".TaskItem").should("have.length", todoItems.length);
   });
-
-  it("should delete task items", () => {
-    addEntries();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".TaskItem").should("have.length", todoItems.length - 2);
-  });
 });
 
-describe("Level 5 milestone task details Page", () => {
+describe("After signing in and visiting the `/tasks` page,", () => {
   beforeEach(() => {
     cy.visit(studentSubmissionUrl);
     cy.get("#username").clear();
@@ -116,41 +99,31 @@ describe("Level 5 milestone task details Page", () => {
     cy.get("button[type='submit']").click();
     cy.visit(studentSubmissionUrl + "/tasks");
   });
-  it("should click and lead to task details page", () => {
+  it("clicking on the task title should navigate and lead to task details page", () => {
     addEntries();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".deleteTaskButton").last().click();
     cy.get(".text-base").click();
     cy.get("h3").should("be.visible");
   });
-  it("should click on item 1 and lead to task details page", () => {
+  it("clicking on the task title should navigate and lead to task details page with the same title rendered in a `<h3>`heading element", () => {
     addEntries();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".text-base").should("have.text", "Sample item 1");
-  });
-  it("should have the right task details", () => {
-    addEntries();
-    cy.get(".deleteTaskButton").last().click();
-    cy.get(".deleteTaskButton").last().click();
     cy.get(".text-base").click();
     cy.get("h3").should("have.text", "Sample item 1");
   });
 });
 
-describe("Level 5 milestone should validate protected route", () => {
-  it("while visiting Homepage and redirect to sign-in page when unauthenticated", () => {
+describe("With protected routes implemented and while the user is not signed in,", () => {
+  it("visiting the Homepage should redirect back to the sign-in page", () => {
     cy.visit(studentSubmissionUrl);
-    cy.location("pathname").should("equal", "/signin"); // Verify that we are redirected to the sign-in page
+    cy.location("pathname").should("equal", "/signin"); 
   });
 
-  it("while visiting Tasks page and redirect to sign-in page when unauthenticated", () => {
+  it("visiting the `/tasks` page should redirect back to the sign-in page", () => {
     cy.visit(studentSubmissionUrl + "/tasks");
-    cy.location("pathname").should("equal", "/signin"); // Verify that we are redirected to the sign-in page
+    cy.location("pathname").should("equal", "/signin");
   });
 });
 
-describe("Level 5 milestone should validate protected route after signin", () => {
+describe("With protected routes implemented and while the user signs in with the username `admin` and password `admin`,", () => {
   beforeEach(() => {
     cy.visit(studentSubmissionUrl);
     cy.get("#username").clear();
@@ -160,18 +133,18 @@ describe("Level 5 milestone should validate protected route after signin", () =>
     cy.get("button[type='submit']").click();
   });
 
-  it("and redirect to Home page when authenticated", () => {
+  it("the application should redirect to the Homepage and the Navigation `<nav>` element should be visible", () => {
     cy.get("nav").should("be.visible");
   });
 
-  it("and redirect to Signin page when clicked on Signout", () => {
+  it("the application should redirect to the `/signin` page if the user signs out", () => {
     cy.visit(studentSubmissionUrl + "/signin");
     cy.visit(studentSubmissionUrl);
     cy.location("pathname").should("equal", "/signin");
   });
 });
 
-describe("Level 5 milestone should validate undefined route", () => {
+describe("With protected routes implemented and while the user signs in with the username `admin` and password `admin`,", () => {
   beforeEach(() => {
     cy.visit(studentSubmissionUrl);
     cy.get("#username").clear();
@@ -181,12 +154,12 @@ describe("Level 5 milestone should validate undefined route", () => {
     cy.get("button[type='submit']").click();
   });
 
-  it("and redirect to NotFound page on invalid path", () => {
+  it("the application redirects to the `/notfound` page on visting an invalid path", () => {
     cy.visit(studentSubmissionUrl + "/test");
     cy.location("pathname").should("equal", "/notfound");
   });
 
-  it("and hide the Header in NotFound page", () => {
+  it("the application does not contain the `<nav>` element on the `/notfound` page", () => {
     cy.visit(studentSubmissionUrl + "/test");
     cy.get("nav").should("have.length", 0);
   });
